e25e06b3a69bb762ad9e084338e45b57bba7138c
Sarwesh Shah <sarweshshah@yahoo.com>
2020-04-25 13:56:48 +0530

    Using a png instead of an svg to reduce reflows

--- sketch.js
+++ sketch.js
@@ -1,18 +1,22 @@
-var state = 0; // Zoom state counter
-
 $(document).ready(function () {
-    $('#container').on('click', function (e) {
+    var state = 0; // Zoom state counter
+
+    $('#container').on('dblclick', function (e) {
         var mouseX = e.pageX;
         var mouseY = e.pageY;
 
+        var svgImg = $('img');
+
         if (state == 0) {
-            $('img').addClass('z1');
+            svgImg.addClass('z1');
             state++;
+
         } else if (state == 1) {
-            $('img').addClass('z2');
+            svgImg.addClass('z2');
             state++;
+
         } else if (state == 2) {
-            $('img').removeClass('z1 z2');
+            svgImg.removeClass('z1 z2');
             state = 0;
         }
     });
